171b02109ba69ba0e7c7122064d98efb5a047595
Archana A <145422353+archanaprajith@users.noreply.github.com>
2025-01-14 09:18:41 +0530

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,28 @@
+
+x=int(input("enter first number"))
+y=int(input("input secound number"))
+a=int(input("enter operations\n 1.add\n 2.sub\n 3.mult\n 4.div"))
+
+if(a==1):
+       print(x+y)
+elif(a==2):
+       print(x-y)
+elif(a==3):
+    print(x*y)
+elif(a==4):
+    print(x/y)
+
+
+      
+
+
+
+
+
+
+
+
+
+
+
+
