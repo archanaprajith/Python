2339b267d5538fbd19ba829485f23cf54c315c09
Archana A <145422353+archanaprajith@users.noreply.github.com>
2025-01-14 09:22:38 +0530

    Add files via upload

---
+++ while2.py
@@ -0,0 +1,27 @@
+x=int(input("enter first number"))
+y=int(input("input secound number"))
+
+ch="z"
+while(ch=="z"):
+
+ a=int(input("enter operations\n 1.add\n 2.sub\n 3.mult\n 4.div"))
+          
+ if(a==1):
+       print(x+y)
+ elif(a==2):
+       print(x-y)
+ elif(a==3):
+    print(x*y)
+ elif(a==4):
+    print(x/y)
+ c=int(input("if need to continue enter 1 else enter 2"))
+ if c==1:
+     ch="c"
+ else :
+    ch="2"
+    
+
+
+
+
+
